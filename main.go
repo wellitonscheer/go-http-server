eff035903a1e9ab254cec2f26114f2effab73588
caie <wellitonscheer@gmail.com>
2024-08-13 17:17:41 -0300

    feat: methods

--- main.go
+++ main.go
@@ -9,10 +9,6 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
-func slash(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "ha\n")
-}
-
 func headers(w http.ResponseWriter, req *http.Request) {
 	for name, headers := range req.Header {
 		for _, h := range headers {
@@ -25,13 +21,20 @@ func increment(num *int) {
 	*num += 1
 }
 
+func setMessage(msg *string, newMsg string) {
+	*msg = newMsg
+}
+
 func main() {
 	a := 0
+	message := "message"
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) { increment(&a); fmt.Fprintf(w, "number a: %v\n", a) })
-	http.HandleFunc("/", slash)
 	http.HandleFunc("/headers", headers)
+
+	http.HandleFunc("GET /message", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "%v\n", message) })
+	http.HandleFunc("POST /message/{msg}", func(w http.ResponseWriter, r *http.Request) { setMessage(&message, r.PathValue("msg")) })
 
 	http.ListenAndServe(":6969", nil)
 }
