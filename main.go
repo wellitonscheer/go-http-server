1be6d207f264ba3782d1b7a762a9d7e172fcdee5
caie <wellitonscheer@gmail.com>
2024-08-13 17:27:19 -0300

    refactor: organize

--- main.go
+++ main.go
@@ -5,15 +5,31 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
+func headers(w http.ResponseWriter, r *http.Request) {
+	for name, headers := range r.Header {
 		for _, h := range headers {
 			fmt.Fprintf(w, "%v: %v\n", name, h)
 		}
+	}
+}
+
+func main() {
+	a := 0
+	message := "message"
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) { increment(&a); fmt.Fprintf(w, "number a: %v\n", a) })
+
+	http.HandleFunc("GET /message", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "%v\n", message) })
+	http.HandleFunc("POST /message/{msg}", func(w http.ResponseWriter, r *http.Request) { setMessage(&message, r.PathValue("msg")) })
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err.Error())
 	}
 }
 
@@ -23,18 +39,4 @@ func increment(num *int) {
 
 func setMessage(msg *string, newMsg string) {
 	*msg = newMsg
-}
-
-func main() {
-	a := 0
-	message := "message"
-
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/increment", func(w http.ResponseWriter, r *http.Request) { increment(&a); fmt.Fprintf(w, "number a: %v\n", a) })
-	http.HandleFunc("/headers", headers)
-
-	http.HandleFunc("GET /message", func(w http.ResponseWriter, r *http.Request) { fmt.Fprintf(w, "%v\n", message) })
-	http.HandleFunc("POST /message/{msg}", func(w http.ResponseWriter, r *http.Request) { setMessage(&message, r.PathValue("msg")) })
-
-	http.ListenAndServe(":6969", nil)
 }
